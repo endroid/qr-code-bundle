b3824b5d52072c79e175bd05f2081f7eeede94b2
endroid <info@endroid.nl>
2018-02-18 15:35:20 +0000

    Swap namespace

--- src/DependencyInjection/EndroidQrCodeExtension.php
+++ src/DependencyInjection/EndroidQrCodeExtension.php
@@ -9,7 +9,7 @@
 
 namespace Endroid\QrCodeBundle\DependencyInjection;
 
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
