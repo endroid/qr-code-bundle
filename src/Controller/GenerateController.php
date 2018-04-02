e5762c20b66626ae68bad96b33d62fc699c2b9b6
endroid <info@endroid.nl>
2018-04-02 12:39:13 +0000

    Rename controller

--- src/Controller/GenerateController.php
+++ src/Controller/GenerateController.php
@@ -16,7 +16,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class QrCodeGenerateController
+class GenerateController
 {
     private $qrCodeFactory;
 
