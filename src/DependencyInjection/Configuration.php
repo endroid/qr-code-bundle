1f03977a415299ea66eef7e22753edc7e94e06b4
Rokas Mikalkėnas <rokas.mikalkenas@nfq.lt>
2019-12-16 12:03:51 +0200

    Fix: wring BC layer for TreeBuilder.

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -23,10 +23,10 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder('endroid_qr_code');
 
-        if (method_exists($treeBuilder, 'root')) {
-            $rootNode = $treeBuilder->root('endroid_qr_code');
-        } else {
+        if (method_exists($treeBuilder, 'getRootNode')) {
             $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $rootNode = $treeBuilder->root('endroid_qr_code');
         }
 
         $rootNode
