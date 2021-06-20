ce9cbb35284bde5174bcefa53129d5cb74829e72
Jeroen van den Enden <info@endroid.nl>
2021-06-20 08:32:00 +0000

    Define controller

--- tests/application/src/Controller/TwigController.php
+++ tests/application/src/Controller/TwigController.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
+#[AsController]
 class TwigController
 {
     private $templating;
