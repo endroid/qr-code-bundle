2cfef4faca424b5b5f1e5c365044f4a1cc7b4c41
Ion Bazan <ion.bazan@gmail.com>
2021-12-07 14:02:50 +0800

    drop old BC layer

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Endroid\QrCodeBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -12,19 +13,13 @@ class Configuration implements ConfigurationInterface
     /** @psalm-suppress PossiblyUndefinedMethod */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        /** @psalm-suppress TooManyArguments */
         $treeBuilder = new TreeBuilder('endroid_qr_code');
-
-        if (method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            /** @psalm-suppress UndefinedMethod */
-            $rootNode = $treeBuilder->root('endroid_documenter');
-        }
+        /** @var ArrayNodeDefinition $rootNode */
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->useAttributeAsKey('name')
-            ->prototype('array')
+            ->arrayPrototype()
             ->prototype('variable')
         ;
 
