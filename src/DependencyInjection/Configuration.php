bf980ed64e76428785cb15f4e217787b1fef0fa5
Jeroen van den Enden <info@endroid.nl>
2021-12-12 14:17:31 +0000

    Remove Symfony 3.4 stuff

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -12,17 +12,10 @@ class Configuration implements ConfigurationInterface
     /** @psalm-suppress PossiblyUndefinedMethod */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        /** @psalm-suppress TooManyArguments */
         $treeBuilder = new TreeBuilder('endroid_qr_code');
 
-        if (method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            /** @psalm-suppress UndefinedMethod */
-            $rootNode = $treeBuilder->root('endroid_documenter');
-        }
-
-        $rootNode
+        $treeBuilder
+            ->getRootNode()
             ->useAttributeAsKey('name')
             ->prototype('array')
             ->prototype('variable')
